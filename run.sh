9309a534e6c2af3bd180c7582a79759a8ed56784
Kerby Tan <kerbytan1234@gmail.com>
2022-07-23 15:45:37 +0800

    regex df

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-
+spark-submit src/app.py \
+-i "./data/httpd/error_log" \
+-o "./output/" \
+--format "text"
--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
+import sys
+import argparse
+import logging, traceback
+import pyspark
 from pyspark.sql import SparkSession
-from pyspark.functions import (
+from pyspark.sql.functions import (
     from_unixtime,
     expr,
     sha2,
@@ -14,4 +18,44 @@ def load_data(format, input):
     if format == 'parquet':
         return spark.read.parquet(input)
     else:
-        return spark.read.txt(input)
+        return spark.read.text(input)
+
+def main(spark):
+
+    #load data
+    raw_df = load_data(flags.format,flags.input)
+    raw_df.show(truncate = False)
+
+    df = raw_df.select( 
+        regexp_extract(col('value'),r'''pid\s(\d+)''',1).alias('PID'),
+        #regexp_extract(col('value'),r'''''')
+    )
+
+    df.show(truncate = False)
+
+    df.write.parquet(flags.output,mode = "overwrite",compression="gzip")
+
+
+if __name__ == "__main__":
+    try:
+        ap = argparse.ArgumentParser()
+        ap.add_argument("-i","--input",help="input path", required=True)
+        ap.add_argument("-o","--output", help="output path", required=True)
+        ap.add_argument(
+            "--format",
+            help="input file format",
+            default="text",
+            choices=["parquet","text"]
+        )
+        
+        flags = ap.parse_args()
+        logging.basicConfig(
+            level=logging.INFO, format="%(asctime)s:%(levelname)s:%(name)s:%(message)s"
+        )
+
+        spark = SparkSession.builder.getOrCreate()
+        main(spark)
+
+    except (Exception):
+        traceback.print_exc()
+        sys.exit(2)
